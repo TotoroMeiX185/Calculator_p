66861f46e36105554545cdabad1c79fef0cea2cf
Sachini Tharanga <sachinitharanga2020al@gmail.com>
2024-08-03 01:33:54 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multifly(x,y):
+    return x*y
+
+def divide(x,y):
+    if y==0:
+        return "Error ! Divided by zero "
+    else:
+        return x/y
+    
+def displa_menu():
+       print("Welcome to Calculator") 
